1db0e5e41ad0b433e428b238e316adc4dd88069b
esjaragit <esjara@siena.cl>
2023-09-07 11:17:56 -0300

    ucentral

--- README.md
+++ README.md
@@ -8,17 +8,75 @@ Proyecto curso Blockchain, creación Dapp.<br><br>
 
 ## PROYECTO REGISTRO MEDICO
 <div align='justify'>
-Mediante un smart contract llamado Ficha, se registra el historial de atenciones, diagnóstico, medicamentos y patologías de las personas. Consistirá en un sistema de registros secuencial para cada usuario, el que activará el consumo de un Token ERC20 (FMD) definido para el registro. Permitirá el acceso al historial de parte del profesional médico enrolado y al agregar información al registro del paciente se realizará un cobro de 1 FMD
+Mediante un smart contract llamado Ficha, se registra el historial de atenciones, diagnóstico, medicamentos y patologías de las personas. Consistirá en un sistema de registros secuencial para cada usuario, el que activará el consumo de un de un contrato definido para el registro. Permitirá el acceso al historial de parte del profesional médico enrolado y al agregar información al registro del paciente.<br><br>
 </div>
-La estructura del registro :
+
+> La estructura del registro :
 - Identificación de la persona
 - Registro de atenciones médicas
 - Registro de Diagnóstico y Tratamientos
 
+## Estructura de contrato
 
+```javascript
+
+struct Record {
+        string patientName;
+        string doctorName;
+        string date;
+        string diagnosis;
+        string problem;
+        string treatment;
+    }
+
+.............................
+
+function addRecord(
+        string memory _patientName,
+        string memory _doctorName,
+        string memory _date,
+        string memory _diagnosis,
+        string memory _problem,
+        string memory _treatment
+    )
+
+..............................
+
+function getRecords() external view returns (Record[] memory) {
+        return medicalRecords[msg.sender];
+```
+
+Para generar el contrato pueden revisar la siguiente url: https://github.com/ejaraU/contrato_medico
+
+## Arquitectura de Software
+
+![image](https://github.com/ejaraU/ucentral/assets/143906202/a93f3a35-bbce-4220-be93-f8784ae705a2)
+
+
+## Pasos de instalación
+
+- Clonar el proyecto desde https://github.com/ejaraU/ucentral.git
+- Agregar la clave privada de tu wallet de Metamask en el archivo '.env'
+- Ejecutar:
+
+```javascript
+
+ npm i -y -f
+
+```
+
+- Ejecutar:
+
+```javascript 
+
+npx http-server
+```
+
+El proceso indicará la dirección y puerto de tu servidor local, para abrir el navegador que tenga instalado Metamask
+
+## Video demostrativo
 
 https://github.com/ejaraU/ucentral/assets/143906202/a1b9596d-9fb2-4d02-bedd-992c54032ce6
-
 
 
 
---
+++ backend/script4.js
@@ -0,0 +1,175 @@
+document.addEventListener("DOMContentLoaded", async () => {
+
+      window.web3 = new Web3(ethereum);
+      
+      const accounts = await web3.eth.getAccounts();
+      web3.eth.defaultAccount = accounts[0];
+initApp();
+
+});
+
+
+
+async function initApp() {
+  const contractAddress = "0xa2CcaAd5e6a21961cAefE8696189A647B3Cb5bE4"; // Replace with your contract address
+  const contractAbi =[
+    {
+      "inputs": [
+        {
+          "internalType": "string",
+          "name": "_patientName",
+          "type": "string"
+        },
+        {
+          "internalType": "string",
+          "name": "_doctorName",
+          "type": "string"
+        },
+        {
+          "internalType": "string",
+          "name": "_date",
+          "type": "string"
+        },
+        {
+          "internalType": "string",
+          "name": "_diagnosis",
+          "type": "string"
+        },
+        {
+          "internalType": "string",
+          "name": "_problem",
+          "type": "string"
+        },
+        {
+          "internalType": "string",
+          "name": "_treatment",
+          "type": "string"
+        }
+      ],
+      "name": "addRecord",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "getRecords",
+      "outputs": [
+        {
+          "components": [
+            {
+              "internalType": "string",
+              "name": "patientName",
+              "type": "string"
+            },
+            {
+              "internalType": "string",
+              "name": "doctorName",
+              "type": "string"
+            },
+            {
+              "internalType": "string",
+              "name": "date",
+              "type": "string"
+            },
+            {
+              "internalType": "string",
+              "name": "diagnosis",
+              "type": "string"
+            },
+            {
+              "internalType": "string",
+              "name": "problem",
+              "type": "string"
+            },
+            {
+              "internalType": "string",
+              "name": "treatment",
+              "type": "string"
+            }
+          ],
+          "internalType": "struct MedicalRecord2.Record[]",
+          "name": "",
+          "type": "tuple[]"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    }
+  ];  
+  
+  const contract = new web3.eth.Contract(contractAbi, contractAddress);
+
+  const addRecordForm = document.getElementById("addRecordForm");
+  const recordsList = document.getElementById("Listado");
+  const prevBtn = document.getElementById("prevBtn");
+  const nextBtn = document.getElementById("nextBtn");
+
+  let userRecords = await contract.methods.getRecords().call({ from: web3.eth.defaultAccount });
+  let currentRecordIndex = 0;
+
+  function showCurrentRecord() {
+    recordsList.innerHTML = ''; // Limpiar la lista de registros
+
+    const currentRecord = userRecords[currentRecordIndex];
+    if (currentRecord) {
+      const recordItem = document.createElement("li");
+      recordItem.innerHTML = `
+        
+        <strong>Paciente:</strong> ${currentRecord.patientName}<br>
+        <strong>Doctor:</strong> ${currentRecord.doctorName}<br>
+        <strong>Fecha:</strong> ${currentRecord.date}<br>
+        <strong>Diagnostico:</strong>
+        <textarea rows = "5" cols = "60" id = "treatment">${currentRecord.diagnosis}</textarea>  
+        <strong>Problema:</strong>
+        <textarea rows = "5" cols = "60" id = "treatment">${currentRecord.problem}</textarea> 
+        <strong>Tratamiento:</strong>
+        <textarea rows = "5" cols = "60" id = "treatment">${currentRecord.treatment}</textarea>  
+      `;
+      recordsList.appendChild(recordItem);
+    }
+  }
+
+  // Mostrar el primer registro al cargar la página
+  showCurrentRecord();
+
+  // Evento para avanzar al siguiente registro
+  nextBtn.addEventListener("click", () => {
+    currentRecordIndex = (currentRecordIndex + 1) % userRecords.length;
+    showCurrentRecord();
+  });
+
+  // Evento para retroceder al registro anterior
+  prevBtn.addEventListener("click", () => {
+    currentRecordIndex = (currentRecordIndex - 1 + userRecords.length) % userRecords.length;
+    showCurrentRecord();
+  });
+
+  addRecordForm.addEventListener("submit", async (event) => {
+    event.preventDefault();
+
+  // Obtener otros campos del formulario
+    
+    
+    const patientName = document.getElementById("patientName").value;
+    const doctorName = document.getElementById("doctorName").value;
+    const date = document.getElementById("fecha").value;
+    const diagnosis = document.getElementById("diagnosis").value;
+    const problem = document.getElementById("problem").value;
+    const treatment = document.getElementById("treatment").value;
+
+    try {
+      await contract.methods
+        .addRecord(patientName, doctorName, date, diagnosis, problem, treatment)
+        .send({ from: web3.eth.defaultAccount });
+      console.log("Registros agregados");
+      // Actualizar la lista de registros aquí
+      userRecords = await contract.methods.getRecords().call({ from: web3.eth.defaultAccount });
+      showCurrentRecord(); // Mostrar el nuevo registro añadido
+    } catch (error) {
+      console.error("Error al cargar Registros:", error);
+    }
+  });
+}
+
+
